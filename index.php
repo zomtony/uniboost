23b4f9994f955079b98fda248200c0ae50568170
yang <zomtony@gmail.com>
2018-07-02 18:16:21 -0700

    deltet img

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 
-
 <?php include($_SERVER['DOCUMENT_ROOT'].'/include/header.php');?>
     <div class="container">       
         <div class="carousel slide" data-ride="carousel" data-type="multi" data-interval="3000" id="myCarousel">
