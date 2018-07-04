98d2db1900ce6c2444a90fb8cb05b250cb881845
Qichen Huang <jeffery@myMac.local>
2018-07-04 00:01:10 -0700

    add footer

--- include/footer.php
+++ include/footer.php
@@ -1,25 +1,20 @@
 		
 		<footer class="footer">
-		 	<div class="row">
-
-                <div class="col-sm-4">
+                <div class="footer-logo">
 					<img class='logo1 logo-color' src='/img/logo1.png' alt = "我是红领巾">
 				</div>
-                <div class="col-sm-6">
-
-					<div class="row btn-margin text-margin-bottom-comment text-center" style="margin-top:15px;">
+				
+				<div class="footer-contact">
+					<div style="height: 40px;">
 						<div class="col-xs-4">关于我们</div>
 						<div class="col-xs-4">联系我们</div>
 						<div class="col-xs-4">用户条款</div>
 					</div>
 
-					<div class="row text-center copyright">
+					<div>
 						<p>Copyright © 2018 All rights reserved</p>
 					</div>        
-
 				</div>
-                <div class="col-sm-2"></div>
-            </div>
 		</footer>	
 	</body>
 </html>
