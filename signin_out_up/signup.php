97ecb1de07e261b9bd664905ac78f59b5bd8057b
Qichen Huang <jeffery@myMac.local>
2018-07-04 21:37:59 -0700

    redesign signup login

--- signin_out_up/signup.php
+++ signin_out_up/signup.php
@@ -10,14 +10,13 @@
 					}
 				?>
 				<div class='row'>
-					<div class="col-lg-1"></div>
-					<div class="col-lg-5">
-						<h1 class="dark-grey">注册</h1>
+					<div class="signup-container">
+						<h1 class="dark-grey"></h1>
 						<div class='row'> 
 							<div class="form-group col-lg-12">
 								<label>用户名<font color="red">*</font></label>
-								<input type="" name="accountf" class="form-control" placeholder="请输入您的登陆账号" required autofocus>
-								<p>请选择您容易记住的账号作为您的登陆用户名</p>
+								<input type="" name="accountf" class="form-control account-form-hint" placeholder="选个好记的账号作为你登陆时的用户名" required autofocus>
+								<!-- <p>请选择一个好记的账号作为你的登陆用户名</p> -->
 							</div>
 						</div>
 						<div class='row'>
@@ -40,10 +39,10 @@
 						</div>
 						<div class='row'>
 							<div class="col-md-6">
-								<input type="submit" id="btnSubmit" class="btn btn-primary" value="创建用户"/>
+								<input type="submit" id="btnSubmit" class="btn btn-primary login-signup-button" value="一键注册"/>
 							</div>
 							<div class='col-md-6 margin-top-s'>
-								<p>已经有了一个账号? <a class="label-link" href="signin.php"> &nbsp 登陆</ a></p >
+								<p>已经有了一个账号? <a class="label-link account-exist" href="signin.php" >登陆</a></p >
 							</div>
 						</div>
 					</div>
