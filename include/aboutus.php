3cfa1fccc1b15c600a968655d7a51daaaa6f80d9
Yang <zomtony@gmail.com>
2018-07-08 16:38:03 -0800

    add footer page link

--- include/aboutus.php
+++ include/aboutus.php
@@ -1,4 +1,13 @@
 <?php include($_SERVER['DOCUMENT_ROOT'].'/include/header.php');?>
+<div class="container">
+        <div class="row text-center theme-backcolor3">
+            <h1>关于我们</h1>
+            <p>我是红领巾，这个公益平台的开发人员。 我想在这里感谢所有支持我们的Tutor们和学生们🙏🏻，还要特别感谢资源狗🐶学姐对我们的大力支持😁<br><br> 
 
+            为了更好的优化这个平台，大家有任何问题或建议都可以在下面给我留言。我们会尽全力把这个平台做得更好☺️ <br><br> 
+
+            再次谢谢你们啦❤️❤️❤️</p>
+        </div>
+    </div>
 
 <?php include($_SERVER['DOCUMENT_ROOT'].'/include/footer.php');?>
