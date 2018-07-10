d2e5a5f69e87a5e27ff3172cf34ecaffa2d5f6d8
Qichen Huang <jeffery@myMac.local>
2018-07-09 18:34:44 -0700

    add terms

--- include/termAndConditions.php
+++ include/termAndConditions.php
@@ -1,4 +1,165 @@
 <?php include($_SERVER['DOCUMENT_ROOT'].'/include/header.php');?>
 
+    <div class="container">
+        <div class="row text-center theme-backcolor3">
+            <h3>用户条款英文版</h3>
+            <pre style='margin: 20px;'>
+
+Terms and Conditions  
+====================
+
+These Terms and Conditions ("Terms", "Terms and Conditions") govern your
+relationship with woshihlj.com (the "Service") operated by
+woshihlj.com ("us", "we", or "our").
+
+Please read these Terms and Conditions carefully before using our woshihlj.com
+web application (the "Service").
+
+Your access to and use of the Service is based on your acceptance of and
+compliance with these Terms. These Terms apply to all visitors, users and
+others who access or use the Service.
+
+By accessing or using the Service you agree to be bound by these Terms and
+accept all legal consequences. If you do not agree to these terms and
+conditions, in whole or in part, please do not use the Service.
+
+  Content  
+-------
+
+Our Service allows you to post, link, store, share and otherwise make
+available certain information, text, graphics, videos, or other material
+("Content"). You are responsible for the Content that you post to the Service,
+including its legality, reliability, and appropriateness.
+
+By posting Content to the Service, you grant us the right and license to use,
+modify, publicly perform, publicly display, reproduce, and distribute such
+Content on and through the Service. You retain any and all of your rights to
+any Content you submit, post or display on or through the Service and you are
+responsible for protecting those rights. You agree that this license includes
+the right for us to make your Content available to other users of the Service,
+who may also use your Content subject to these Terms.
+
+You represent and warrant that: (i) the Content is yours (you own it) or you
+have the right to use it and grant us the rights and license as provided in
+these Terms, and (ii) the posting of your Content on or through the Service
+does not violate the privacy rights, publicity rights, copyrights, contract
+rights or any other rights of any person.
+
+We reserve all rights to block or remove communications or materials that we
+determine to be: (a) abusive, defamatory, or obscene; (b) fraudulent,
+deceptive, or misleading; (c) in violation of a copyright, trademark or, other
+intellectual property right of another or; (d) offensive or otherwise
+unacceptable to us in our sole discretion.
+
+You acknowledge that, by providing you with the ability to view and distribute
+user-generated content on the Service, we are merely acting as a passive
+conduit for such distribution and is not undertaking any obligation or
+liability relating to any contents or activities on the Service.
+
+  Accounts  
+--------
+
+When you create an account with us, you must provide us information that is
+accurate, complete, and current at all times. Failure to do so constitutes a
+breach of the Terms, which may result in immediate termination of your account
+on our Service.
+
+You are responsible for safeguarding the password that you use to access the
+Service and for any activities or actions under your password, whether your
+password is with our Service or a third-party service.
+
+You agree not to disclose your password to any third party. You must notify us
+immediately upon becoming aware of any breach of security or unauthorized use
+of your account.
+
+You may not use as a username the name of another person or entity or that is
+not lawfully available for use, a name or trade mark that is subject to any
+rights of another person or entity other than you without appropriate
+authorization, or a name that is otherwise offensive, vulgar or obscene.
+
+ Links To Other Web Sites  
+------------------------
+
+The Service may contain links to third-party web sites or services that are
+not owned or controlled by woshihlj.com.
+
+woshihlj.com has no control over, and assumes no responsibility for, the content,
+privacy policies, or practices of any third party web sites or services. You
+further acknowledge and agree that woshihlj.com shall not be responsible or
+liable, directly or indirectly, for any damage or loss caused or alleged to be
+caused by or in connection with use of or reliance on any such content, goods
+or services available on or through any such websites or services.
+
+We strongly advise you to read the terms and conditions and privacy policies
+of any third-party web sites or services that you visit.
+
+  Termination  
+-----------
+
+We may terminate or suspend access to our Service immediately, without prior
+notice or liability, for any reason whatsoever, including, without limitation,
+if you breach the Terms.
+
+All provisions of the Terms shall survive termination, including, without
+limitation, ownership provisions, warranty disclaimers, indemnity and
+limitations of liability.
+
+Upon termination, your right to use the Service will immediately cease. If you
+wish to terminate your account, you may simply discontinue using the Service.
+
+  Governing Law  
+-------------
+
+These Terms shall be governed by, and interpreted and enforced in accordance
+with, the laws in the Province of British Columbia and the laws of Canada, as
+applicable.
+
+If any provision of these Terms is held to be invalid or unenforceable by a
+court of competent jurisdiction, then any remaining provisions of these Terms
+will remain in effect. These Terms constitute the entire agreement between us
+regarding our Service, and supersede and replace any prior agreements, oral or
+otherwise, regarding the Service.
+
+  Changes  
+-------
+
+We reserve the right, at our sole discretion, to modify or replace these Terms
+at any time. What constitutes a material change will be determined at our sole discretion.
+
+By continuing to access or use our Service after those revisions become
+effective, you agree to be bound by the revised terms. If you do not agree to
+the new terms, in whole or in part, please stop using the website and the
+Service.
+
+
+  Safety
+------
+
+Your Interactions with Other Users.
+Though woshihlj.com strives to encourage a respectful user experience through features like 
+Looking for Tutors and Looking for Students that only allows users to communicate if they have 
+both indicated interest in one another, it is not responsible for the conduct of any user on or 
+off of the Service. You agree to use caution in all interactions with other users, particularly 
+if you decide to communicate off the Service or meet in person. Prior to using the Service.
+You agree you will not provide your financial information (for example, your credit card or
+bank account information), or wire or otherwise send money, to other users.
+
+YOU ARE SOLELY RESPONSIBLE FOR YOUR INTERACTIONS WITH OTHER USERS. YOU UNDERSTAND THAT WOSHIHLJ.COM 
+DOES NOT CONDUCT CRIMINAL BACKGROUND CHECKS ON ITS USERS OR OTHERWISE INQUIRE INTO THE BACKGROUND OF ITS USERS. 
+WOSHIHLJ.COM MAKES NO REPRESENTATIONS OR WARRANTIES AS TO THE CONDUCT OF USERS. WOSHIHLJ.COM RESERVES THE RIGHT 
+TO CONDUCT - AND YOU AGREE WOSHIHLJ.COM MAY CONDUCT - ANY CRIMINAL BACKGROUND CHECK OR OTHER SCREENINGS 
+AT ANY TIME USING AVAILABLE PUBLIC RECORDS.
+
+
+   Contact Us  
+----------
+
+If you have any questions about these Terms, please contact us.
+woshihlj.com@gmail.com
+
+
+            </pre>
+        </div>
+    </div>
 
 <?php include($_SERVER['DOCUMENT_ROOT'].'/include/footer.php');?>
