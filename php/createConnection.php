edc1fb861111e799e4b5ca040e344dd520fe6359
Qichen Huang <jeffery@myMac.local>
2018-07-10 17:13:17 -0700

    new branch

--- php/createConnection.php
+++ php/createConnection.php
@@ -1,10 +1,10 @@
 <?php
 	class createConnection {
-		public $servername = "178.128.12.124";
+		public $servername = "45.78.25.200";
 		public $port = "3306";
-		public $username = "zhimacollege";
-		public $password = "Uniboost12";
-		public $dbname = "zhimacollege";
+		public $username = "zhimadeveloper";
+		public $password = "zhimadeveloper12";
+		public $dbname = "zhimadeveloper";
 		public $conn;
 		// Check connection
 		function connect(){
