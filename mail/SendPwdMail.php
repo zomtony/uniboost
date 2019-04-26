5f13c26afba8291d96c90446ad75a7dd4f9c4e5a
Yang Tong <ytong13@my.bcit.ca>
2019-04-26 16:09:40 -0700

    add find password function

--- mail/SendPwdMail.php
+++ mail/SendPwdMail.php
@@ -37,8 +37,8 @@
 				$mail->isSMTP();                                      // Set mailer to use SMTP
 				$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 				$mail->SMTPAuth = true;                               // Enable SMTP authentication
-				$mail->Username = 'no.replay.woshihlj@gmail.com';                 // SMTP username
-				$mail->Password = 'Uniboost12';                           // SMTP password
+				$mail->Username = '****';                 // SMTP username
+				$mail->Password = '****';                           // SMTP password
 				$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 				$mail->Port = 587;                                    // TCP port to connect to
 		
